7a073e18215167d2d943ba75287aa955fad276b7
ukmothukuri <ukmothukuri@gmail.com>
2024-10-25 17:34:00 +0530

    commit 25th evening

--- src/exceptions/ExceptionDemo.java
+++ src/exceptions/ExceptionDemo.java
@@ -1,14 +1,9 @@
 package exceptions;
-
 import java.io.*;
-
 public class ExceptionDemo {
-
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		System.out.println("Enter name");
 		String name = br.readLine(); // reads a line of text
 		System.out.println("Your Good Name: "+name);

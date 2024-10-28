708d8e2e220d3cf5e57735b18a224ffe09fe56da
ukmothukuri <ukmothukuri@gmail.com>
2024-10-28 17:44:08 +0530

    multithreading - commit

---
+++ src/multithreading/Multi.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+public class Multi extends Thread {
+
+	@Override
+	public void run() {
+		 for(int i=1; i<=25; i++) {
+			 System.out.println(i);
+			 try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Multi t1 = new Multi(); // new 
+		t1.start(); // Runnable
+		
+		Multi t2 = new Multi(); // new 
+		t2.start(); // Runnable
+		
+		
+	}
+}
